8cb41b9172a60509378fc07daad0e7778fd62146
ArthurDenis <83367368+ArthurDenis@users.noreply.github.com>
2022-06-14 15:04:36 +0200

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 const app = express();
 const path = require('path');
+const helmet = require('helmet');
+require('dotenv').config();
 
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
 
 
-mongoose.connect('',
+mongoose.connect(process.env.SECURE_DB,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -18,6 +20,8 @@ mongoose.connect('',
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use(express.json());
+
+app.use(helmet())
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
